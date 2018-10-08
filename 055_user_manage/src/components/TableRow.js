eda19b6e4a32eb8c06725bb8d113c68094c69273
Lap Tran <lapth82@gmail.com>
2018-10-09 06:32:29 +0700

    search functionality

---
+++ 055_user_manage/src/components/TableRow.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class TableRow extends Component {
+    render() {
+        return (
+            <tr>
+                <td>{this.props.rowData.id}</td>
+                <td>{this.props.rowData.hoTen}</td>
+                <td>{this.props.rowData.tel}</td>
+                <td>{this.props.rowData.quyen}</td>
+                <td>
+                    <div className="btn-group">
+                        <div className="btn btn-warning"><i className="fa fa-edit" /> Sửa</div>
+                        <div className="btn btn-danger"><i className="fa fa-trash" /> Xóa</div>
+                    </div>
+                </td>
+            </tr>
+        );
+    }
+}
+
+export default TableRow;
